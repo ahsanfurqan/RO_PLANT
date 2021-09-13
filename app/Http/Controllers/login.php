03ea1aa6b28ece1d844f37f288b6c836e36246df
ahsanfurqan <ahsanfurqan.bukc@gmail.com>
2021-09-13 19:07:03 +0500

    login changes

--- app/Http/Controllers/login.php
+++ app/Http/Controllers/login.php
@@ -10,11 +10,13 @@ use Illuminate\Support\Facades\Auth;
 class login extends Controller
 {
     public function userlogin(Request $request){
-        $email=$request->email;
-        $password=$request->password;
+        // $email=$request->email;
+        // $password=$request->password;
+        
         
         $data=$request->only(['email','password']);
         $bool=Auth::attempt($data);
-        return response()->json(['message'=>$bool]);
+        return response()->json(['status_message'=>$bool]);
+        
     }
 }
